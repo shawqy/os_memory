6b15feae2dd0ef8a8397f354e287fc4d190636e8
samirMossad1 <samirmossad6@gmail.com>
2019-05-04 18:52:32 +0200

    update best_fit

--- best_fit.cpp
+++ best_fit.cpp
@@ -1,8 +1,94 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <string>
+#include "hole.h"
+
+using namespace std;
+int best_fit(list <hole> &memory, int * sizelist, string * namelist, int ProcessNumber);
+
+bool hole_size(hole,hole);
+
+
+
+int main()
+{
+
+ list<hole> memory;
+ 
+ hole h1,h2,h3,h4,h5;
+ string x="SS";
+ int y=3;
+ 
+ 
+ h1.set_ID(1);
+ h1.set_StartingAddress(100);
+ h1.set_endAdress(150);
+ h1.set_name("hole1");
+ h1.set_flag(0);
+
+
+ h2.set_ID(2);
+ h2.set_StartingAddress(200);
+ h2.set_endAdress(225);
+ h2.set_name("hole2");
+ h2.set_flag(0);
+
+
+ h3.set_ID(3);
+ h3.set_StartingAddress(300);
+ h3.set_endAdress(400);
+ h3.set_name("hole3");
+ h3.set_flag(1);
+
+
+
+ h4.set_ID(4);
+ h4.set_StartingAddress(400);
+ h4.set_endAdress(800);
+ h4.set_name("hole4");
+ h4.set_flag(0);
+
+
+ h5.set_ID(5);
+ h5.set_StartingAddress(300);
+ h5.set_endAdress(400);
+ h5.set_name("hole5");
+ h5.set_flag(1);
+
+memory.push_back(h1);
+
+memory.push_back(h2);
+
+memory.push_back(h3);
+
+memory.push_back(h4);
+
+memory.push_back(h5);
+
+//best_fit(memory,&y,&x,2);
+
+
+    return 0;
+}
+
+
 int best_fit(list <hole> &memory, int * sizelist, string * namelist,int NumberOFSegments,int ProcessNumber)
 {
     
     list<hole> copyMemory;
     list<hole>::iterator myit;
+    list<hole>::iterator myitMemory;
     
 
 /*Parse The holes*/    
@@ -103,13 +189,43 @@ for(int i=0;i<NumberOFSegments;i++)
 }
 
 
+
         /*Copy the state of the copyMemory to the RealMemory*/
         for(myit=copyMemory.begin();myit!=copyMemory.end();myit++)
         {
     
             
-            
-            
+            for(myitMemory=memory.begin();myitMemory!=memory.end();myitMemory++)
+            {
+                
+                //loop till you find the hole with the same starting address
+                if(myit->get_StartingAddress() == myitMemory->get_StartingAddress())
+                {
+                    
+                    //Copy the info from the hole to the memory even if it is not adjusted
+                    myitMemory->set_endAdress(myit->get_endAdress());
+                    myitMemory->set_ID(myit->get_ID());
+                    myitMemory->set_name(myit->get_name());
+                    myitMemory->set_flag(myit->get_flag());
+                    
+                    //we find the hole no need to continue for this hole
+                    break;
+                }
+                
+                //if another hole is added
+                else if(myitMemory->get_flag()==0)
+                {
+                    
+                    memory.insert(myitMemory,(*myit));
+                    
+                    //no need to continue we already found the hole place 
+                    break;
+                    
+                }
+                
+                
+            }
+    
             
             
         }
